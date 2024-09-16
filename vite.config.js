8b7ec2b228f471166e2991ae42664f06cc6e9ae5
Danyil <danilagarma1999@mail.ru>
2024-09-16 17:16:50 +0300

    almost finish header

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import eslintPlugin from 'vite-plugin-eslint';
+import path from 'path';
+import { fileURLToPath } from 'url'; // Импортируем для работы с __dirname
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// Определяем __dirname
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  console.log('Vite mode:', mode); // Логируем текущий режим (development или production)
+
+  return {
+    plugins: [react(), eslintPlugin()],
+    build: {
+      sourcemap: !isProduction, // Source Maps включены только в dev-режиме
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@styles': path.resolve(__dirname, './src/styles'),
+        '@fonts': path.resolve(__dirname, './src/assets/fonts'),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @use "@styles/variables.scss";
+            @use "@styles/mixins.scss";
+          `,
+        },
+      },
+    },
+  };
+});
